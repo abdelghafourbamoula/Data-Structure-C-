1be2de01c8f52f4308ee9175aad23806951f781e
Abdelghafour Bamoula <81409058+abdelghafourbamoula@users.noreply.github.com>
2022-07-19 13:51:05 +0100

    linked lists

---
+++ doubleLinkedListes.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* createNode(int data){
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->prev = NULL;
+    node->next = NULL;
+
+    return node;
+}
+
+void printList(struct Node* node) {
+    
+    while (node != NULL) {
+        printf("%d", node->data);
+
+        if(node->next != NULL)
+            printf(" -> ");
+
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void printInv(struct Node* head) {
+    struct Node* node = head;
+
+    while (node->next != NULL) {
+        node = node->next;
+    }
+
+    while (node != NULL) {
+        printf("%d", node->data);
+
+        if(node->prev != NULL)
+            printf(" -> ");
+
+        node = node->prev;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head, int data){
+    
+    if (*head != NULL) {    
+        struct Node* node = createNode(data);
+        node->next = (*head);
+        (*head)->prev = node;
+        (*head) = node;
+    }
+    else {
+        printf("the list is empty ...");
+    }
+}
+
+void append(struct Node* head, int data){
+
+    while (head->next != NULL){
+        head = head->next;
+    } 
+
+    struct Node* node = createNode(data);
+    node->prev = head;
+    node->next = NULL;
+
+    head->next = node;
+}
+
+void pop(struct Node** head) {
+    if ((*head)->next != NULL && (*head)->prev == NULL) {
+        (*head)->next->prev = NULL;
+        *head = (*head)->next;
+    }
+    else {
+        printf("The list is empty or has another head ...");
+    }
+}
+
+void popLast(struct Node* head) {
+    while (head->next != NULL) {
+        head = head->next;
+    }
+
+    head->prev->next = NULL;
+    head->prev = NULL;
+}
+
+void delete(struct Node* head, int data) {
+    if (head->data == data){
+        pop(&head);
+    }
+    else {
+        while (head != NULL){
+
+            if (head->data == data) {
+                if (head->next == NULL){
+                    popLast(head);
+                    break;
+                }
+
+                head->prev->next = head->next;
+                head->next->prev = head->prev;
+                head->next = NULL;
+                head->prev = NULL;
+            }
+
+            head = head->next;
+        }    
+    } 
+}
+
+void insertAfter(struct Node* head, int data, int value) {
+
+    while (head->next != NULL) {
+        if (head->data == data) {
+            struct Node* node = createNode(value);
+            node->next = head->next;
+            head->next->prev = node;
+            head->next = node;
+            node->prev = head;
+            return;
+        }
+        head = head->next;
+    }
+
+    if (head->data == data) {
+        append(head, value);
+    }    
+}
+
+void insertBefore(struct Node* head, int data, int value) {
+   
+    while (head->next != NULL) {
+        if (head->data == data) {
+            struct Node* node = createNode(value);
+            head->prev->next = node;
+            node->prev = head->prev;
+            node->next = head;
+            head->prev = node;
+            return;
+        }
+        head = head->next;
+    }
+}
+
+void InsertBetween(struct Node* head, int after, int before, int value) {
+    while (head->next != NULL) {
+        if (head->data == after && head->next->data == before){
+            insertAfter(head, after, value);
+        }
+        head = head->next;
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->prev = NULL;
+    head->next = second;
+
+    second->data = 2;
+    second->prev = head;
+    second->next = third;
+
+    third->data = 3;
+    third->prev = second;
+    third->next = NULL;
+
+    // print List
+    printf("Forward: ");
+    printList(head);
+    printf("Backward: ");
+    printInv(head);
+
+    push(&head, 0);
+    printList(head);
+    // printInv(head);
+
+    append(head, 4);
+    printList(head);
+
+    pop(&head);
+    printList(head);
+    // printInv(head);
+
+    popLast(head);
+    printList(head);
+
+    delete(head,2);
+    printList(head);
+
+    insertAfter(head,1,22);
+    printList(head);
+    insertAfter(head,3,4);
+    printList(head);
+
+    insertBefore(head,22,2);
+    printList(head);
+
+    InsertBetween(head,1,2,11);
+    printList(head);
+    InsertBetween(head,3,4,33);
+    printList(head);
+
+    return 0;
+}
---
+++ linkedList.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+
+void printList(struct Node* node){
+    while (node != NULL)
+    {
+        printf("%d -> ", node->data);
+
+        if (node->next == NULL){
+            printf("null\n");
+        }
+        
+        node = node->next;
+    }
+    
+}
+
+void push( struct Node** head, int data){
+    if (head != NULL){
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        new_node->data = data;
+        new_node->next = (*head);
+        (*head) = new_node;
+    }
+    else{
+        printf("the list is empty ...");
+    }
+}
+
+void insertAfter(struct Node* prev_node, int data){
+    if (prev_node != NULL) {
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        new_node->data = data;
+        new_node->next = prev_node->next;
+        prev_node->next = new_node;
+    }
+    else {
+        printf("the previous node could not be null ...");
+    }
+}
+
+void insertAfterValue(struct Node* node, int prev, int data){
+    while(node != NULL){
+        if(node->data == prev){
+            struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+            new_node->data = data;
+            new_node->next = node->next;
+            node->next = new_node;
+            break;
+        }
+        node = node->next;
+    } 
+    
+}
+
+void append(struct Node* node, int data){
+    
+    while(node->next != NULL){
+        node = node->next;
+    }
+
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    node->next = new_node;
+}
+
+void pop(struct Node** head){
+    struct Node* first = *head;
+
+    if (first->next != NULL){
+        *first = *first->next;
+    }
+    else {
+        printf("the list is empty ...");
+    }
+    
+}
+
+void popLast(struct Node* head){
+
+    while (head != NULL){
+        if (head->next->next == NULL) {
+            head->next = NULL;    
+        }
+
+        head = head->next;
+    }
+    
+}
+
+void delete(struct Node* head, int data) {
+
+    if (head!=NULL && head->data==data) {
+        free(head);
+        return;
+    }
+    
+    
+    while (head->next != NULL) {
+        if(head->next->data == data){
+            head->next = head->next->next;
+            break;
+        }
+        
+        head = head->next;
+    }
+    
+}
+
+void deleteCenter(struct Node* head) {
+    int length = 0;
+    struct Node* node = head;
+
+    while (node != NULL) {
+        length ++;
+        node = node->next;
+    }
+    
+    if (length > 2) {
+        int i=0;
+
+        while (head != NULL) {  
+           
+            if (i == length/2 - 1) {
+                head->next = head->next->next;
+                break;
+            }
+            i++;
+            head = head->next;
+        }
+    }
+    else {
+        printf("the list should have more than a2 elements for delete the center ...");
+    } 
+    
+}
+
+
+int main(int argc, char const *argv[])
+{
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    second->data = 2;
+    third->data = 3;
+
+    head->next = second;
+    second->next = third;
+    third->next = NULL;
+    printList(head);
+
+    // insert on the first 
+    push(&head, 0);
+    printList(head);
+
+    // insert after node
+    insertAfter(second,22);
+    printList(head);
+
+    //  insert after value 
+    insertAfterValue(head,1,33);
+    printList(head);
+
+    // append at the end 
+    append(head, 4);
+    printList(head);
+
+    // pop first element
+    pop(&head);
+    printList(head);
+
+    // pop last element 
+    popLast(head);
+    printList(head);
+
+    // delete the middle node 
+    deleteCenter(head);
+    printList(head);
+
+    // delete value 
+    delete(head, 33);
+    printList(head);
+
+    return 0;
+}
